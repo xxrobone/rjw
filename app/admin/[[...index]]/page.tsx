351e351ec6ca4c020b838d755c3bed6d15fafcf6
robonexx <robertwagar@gmail.com>
2023-03-02 08:12:33 +0100

    new project using next and sanity

---
+++ app/admin/[[...index]]/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { NextStudio } from 'next-sanity/studio';
+import { config } from '../../../sanity.config';
+
+// importing all setting from config file to be used and passed to the Next app
+
+export default function StudioPage() {
+    return <NextStudio config={config} />
+};
---
+++ sanity.config.ts
@@ -0,0 +1,9 @@
+import { deskTool } from 'sanity/desk'
+
+export const config = {
+    projectId: "ygg99ic5",
+    dataset: "production",
+    title: "rjw",
+    basePath: "/admin",
+    plugins: [deskTool()],
+}
