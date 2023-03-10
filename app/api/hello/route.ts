281429e8bc8c7d3642bb9646f8ffe1376894fe2d
robonexx <robertwagar@gmail.com>
2023-03-10 13:36:36 +0100

    ...

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,3 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+export async function GET(request: Request) { // eslint-disable-line @typescript-eslint/no-unused-vars
+  return new Response('Hello, Next.js!') // eslint-disable-line @typescript-eslint/no-unused-vars
 }
