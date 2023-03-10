1e4d54a68d121e770b36d63d40099a832a329b1c
robonexx <robertwagar@gmail.com>
2023-03-10 13:35:08 +0100

    oh wow

--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -1,6 +1,6 @@
-'use client';
-import { NextStudio } from 'next-sanity/studio';
-import { config } from '../../../sanity.config';
+'use client'
+import { NextStudio } from 'next-sanity/studio'
+import { config } from '../../../sanity.config'
 
 // importing all setting from config file to be used and passed to the Next app
 
