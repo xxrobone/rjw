f047e8e6d3b67b3005c4200b67e8956bfb6f387d
robonexx <robertwagar@gmail.com>
2023-03-10 14:59:22 +0100

    adding data for project page

--- assets/data/pageData.js
+++ assets/data/pageData.js
@@ -25,3 +25,77 @@ export const navData = [
     id: 5,
   },
 ];
+
+export const additionalData = [
+  {
+    name: "Robert Wägar",
+    avatar: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg",
+    links: [
+        {
+            title: "Check my website",
+            href: "https://robertwagar.se",
+            image: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg"
+        },
+        {
+            title: "lockerlegends",
+            href: "https://lockerlegends.org",
+            image: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg"
+        }
+    ],
+    socials: [
+        {
+            title: "twitter",
+            href: "https://twitter.com/Rob2da1"
+        },
+        {
+            title: "github",
+            href: "https://github.com/robonexx"
+        },
+        {
+            title: "codepen",
+            href: "https://codepen.io/robonexx"
+        },
+        {
+            title: "linkedin",
+            href: "https://www.linkedin.com/in/robert-w%C3%A4gar-1b4661139/"
+        }
+    ],
+    projects: [
+      {
+          title: "Assignment 1",
+          href: "https://rob_assignment_one.surge.sh/",
+          github: "https://github.com/xxrobone/framew_assingment1_week1_cme",
+          image: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg"
+      },
+      {
+          title: "Assignment 2",
+        href: "https://github.com/xxrobone/framew_assingment2_week2_cme",
+        github: "https://github.com/xxrobone/framew_assingment2_week2_cme",
+          image: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg"
+      },
+      {
+          title: "Assignment 3",
+        href: "https://massdestruction.vercel.app/",
+        github: "https://github.com/xxrobone/framew_assingment3_week3_cme",
+          image: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg"
+      },
+      {
+          title: "Assignment 4",
+        href: "https://framew-assingment4-week4-cme.vercel.app/",
+        github: "https://github.com/xxrobone/framew_assingment4_week4_cme",
+          image: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg"
+      },
+      {
+          title: "Assignment 5",
+        href: "https://framew-assingment5-week5-cme.vercel.app/",
+        github: "https://github.com/xxrobone/rjw",
+          image: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg"
+      },
+      {
+          title: "lockerlegends",
+          href: "https://lockerlegends.org",
+          image: "https://pbs.twimg.com/profile_images/1612739344682426369/cyzqToP3_400x400.jpg"
+      }
+  ],
+}
+]
