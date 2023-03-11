5b86f68711abfb13857d3cd335749835bc78f45e
robonexx <robertwagar@gmail.com>
2023-03-11 10:31:14 +0100

    eslint

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,6 @@
-export async function GET(request: Request) { // eslint-disable-line @typescript-eslint/no-unused-vars
-  return new Response('Hello, Next.js!') // eslint-disable-line @typescript-eslint/no-unused-vars
+
+// eslint-disable-next-line no-unused-vars
+export async function GET(request: Request) {
+  // eslint-disable-next-line no-unused-vars
+  return new Response('Hello, Next.js!')
 }
