533d9c332253aa175abe7e6da97ea3360a76f544
robonexx <robertwagar@gmail.com>
2023-03-12 11:55:17 +0100

    ...

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,12 +1,12 @@
 
 /* import React, { useState, useRef } from 'react' */
-import Image from 'next/image';
+/* import Image from 'next/image'; */
 /* import { useRouter } from 'next/navigation'; */
 /* import emailjs from '@emailjs/browser'; */
 /* import {motion} from 'framer-motion' */
-import { FcCheckmark } from 'react-icons/fc'
+/* import { FcCheckmark } from 'react-icons/fc' */
 /* import Modal from '../components/modal/Modal'; */
-import IMG from '/public/images/robIcon.png'
+/* import IMG from '/public/images/robIcon.png' */
 import { socials } from '@/assets/data/pageData'
 import LinkCard from '../components/linkcard/LinkCard'
 
