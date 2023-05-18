70c32ba7409c32bbd0838aaf896dc2d952d6a575
xxrobone <robert.wagar@edu.cmeducations.se>
2023-05-18 17:16:38 +0200

    removed video, eraser done

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -1,32 +1,127 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
-
+import useWindowSize from '../../hooks/useWindowSize';
 // styles
 import styles from './Hero.module.scss';
 
 const Vid = () => (
-  <video
-    autoPlay
-    loop
-    muted
-    playsInline
-    fill
-    className={styles.video}
-  >
-    <source src='/earth.webm' type='video/mp4' />
+  <div className={styles.video}>
+  <video autoPlay loop muted playsInline>
+    <source src='/colors.mp4' type='video/mp4' />
   </video>
+  </div>
 );
 
+const parentVariant = {
+  initial: { y: 800 },
+  animate: {
+    y: 0,
+    transition: {
+      staggerChildren: 0.2,
+      delayChildre: 0.3,
+    },
+  },
+};
+
+const childVariant = {
+  initial: { y: 800 },
+  animate: {
+    y: 0,
+    transition: {
+      duration: 1,
+      ease: [0.5, 0.04, -0.01, 0.8],
+    },
+  },
+};
+
 const Hero = () => {
+  const size = useWindowSize();
+  const [currentT, setCurrent] = useState('#000');
+
+  let canvas = useRef(null);
+  useEffect(() => {
+    let renderingElement = canvas.current;
+    // create an offscreen canvas only for the drawings
+    let drawingElement = renderingElement.cloneNode();
+    let drawingCtx = drawingElement.getContext('2d');
+    let renderingCtx = renderingElement.getContext('2d');
+    let lastX;
+    let lastY;
+    let moving = false;
+
+    renderingCtx.globalCompositeOperation = 'source-over';
+    renderingCtx.fillStyle = '#000000';
+    renderingCtx.fillRect(0, 0, size.width, size.height);
+
+    renderingElement.addEventListener('mouseover', (ev) => {
+      moving = true;
+      lastX = ev.pageX - renderingElement.offsetLeft;
+      lastY = ev.pageY - renderingElement.offsetTop;
+    });
+
+    renderingElement.addEventListener('click', (ev) => {
+      moving = true;
+      lastX = ev.pageX - renderingElement.offsetLeft;
+      lastY = ev.pageY - renderingElement.offsetTop;
+    });
+
+    renderingElement.addEventListener('mouseup', (ev) => {
+      moving = false;
+      lastX = ev.pageX - renderingElement.offsetLeft;
+      lastY = ev.pageY - renderingElement.offsetTop;
+    });
+
+    renderingElement.addEventListener('mousemove', (ev) => {
+      if (moving) {
+        drawingCtx.globalCompositeOperation = 'source-over';
+        renderingCtx.globalCompositeOperation = 'destination-out';
+        let currentX = ev.pageX - renderingElement.offsetLeft;
+        let currentY = ev.pageY - renderingElement.offsetTop;
+        drawingCtx.lineJoin = 'round';
+        drawingCtx.moveTo(lastX, lastY);
+        drawingCtx.lineTo(currentX, currentY);
+        drawingCtx.closePath();
+        drawingCtx.lineWidth = 80;
+        drawingCtx.stroke();
+        lastX = currentX;
+        lastY = currentY;
+        renderingCtx.drawImage(drawingElement, 0, 0);
+      }
+    });
+  }, [currentT]);
   return (
-    <div className={styles.hero}>
+    <motion.div className={styles.hero}>
+      <canvas
+        className={styles.canvas}
+        ref={canvas}
+        height={size.height}
+        width={size.width}
+      ></canvas>
       <Vid />
-      <h1 className={styles.title}>
-        Rupert<span>Von</span>
-        <span>Code</span>
-      </h1>
-    </div>
+      <motion.h1
+        className={styles.title}
+        variants={parentVariant}
+        initial='initial'
+        animate='animate'
+      >
+        Rupert
+        <motion.span
+          variants={childVariant}
+          initial='initial'
+          animate='animate'
+        >
+          Von
+        </motion.span>
+        <motion.span
+          variants={childVariant}
+          initial='initial'
+          animate='animate'
+        >
+          Code
+        </motion.span>
+      </motion.h1>
+    </motion.div>
   );
 };
 
--- app/hooks/useWindowSize.js
+++ app/hooks/useWindowSize.js
@@ -1,23 +1,23 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 
 export default function useWindowSize() {
   function getSize() {
     return {
       width: window.innerWidth,
       height: window.innerHeight,
-    }
+    };
   }
 
-  const [windowSize, setWindowSize] = useState(getSize)
+  const [windowSize, setWindowSize] = useState(getSize);
 
   useEffect(() => {
     function handleResize() {
-      setWindowSize(getSize())
+      setWindowSize(getSize());
     }
 
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-  return windowSize
+  return windowSize;
 }
